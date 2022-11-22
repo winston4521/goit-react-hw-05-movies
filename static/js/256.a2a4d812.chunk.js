2322564da482aa95a60b54cbe665b81a3bc653dd
winston4521 <46812129+winston4521@users.noreply.github.com>
2022-11-22 21:32:43 +0000

    Deploying to gh-pages from @ winston4521/goit-react-hw-05-movies@9d860bd84a6329194f26f483294983d9ce46c428 🚀

---
+++ static/js/256.a2a4d812.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[256],{256:function(t,e,n){n.r(e),n.d(e,{NotFound:function(){return o}});var u=n(184),o=function(){return(0,u.jsx)("div",{children:"404 Not Found"})};e.default=o}}]);
+//# sourceMappingURL=256.a2a4d812.chunk.js.map
