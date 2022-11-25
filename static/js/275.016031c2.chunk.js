475c596b81728e48af773f83a7ebe8297f3cbdb3
winston4521 <46812129+winston4521@users.noreply.github.com>
2022-11-25 13:12:12 +0000

    Deploying to gh-pages from @ winston4521/goit-react-hw-05-movies@145ae7636bc9acfa9b8faa500d33b339a927ae8e 🚀

---
+++ static/js/275.016031c2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[275],{275:function(o,t,n){n.r(t),n.d(t,{NotFound:function(){return r},default:function(){return s}});var u={NotFoundWrap:"NotFount_NotFoundWrap__Omti1",NotFount:"NotFount_NotFount__fshbI"},e=n(184),r=function(){return(0,e.jsx)("div",{className:u.NotFoundWrap,children:(0,e.jsx)("h2",{className:u.notFount,children:" Ooops 404 Not Found Page"})})},s=r}}]);
+//# sourceMappingURL=275.016031c2.chunk.js.map
